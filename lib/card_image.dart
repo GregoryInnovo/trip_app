4383e369a3d9e1676b70abeba861f53e5923a89d
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-01-01 23:01:14 -0500

    floating action button with statefull widget

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'floating_action_button_green.dart';
 
 class CardImage extends StatelessWidget {
   String pathImage = "assets/img/place_1.jpg";
@@ -24,6 +25,9 @@ class CardImage extends StatelessWidget {
           ]),
     );
 
-    return card;
+    return Stack(alignment: const Alignment(0.9, 1.1), children: <Widget>[
+      card,
+      FloatingActionButtonGreen(),
+    ]);
   }
 }
